d6883e11a87273f793842c619a0a7f052b3dff2a
annavvoronina <annavvoronina@yandex.ru>
2023-06-11 21:20:00 +0300

    Этап 2. Основной сервис

--- ewm-service/src/main/java/ru/practicum/request/controller/RequestController.java
+++ ewm-service/src/main/java/ru/practicum/request/controller/RequestController.java
@@ -39,7 +39,7 @@ public class RequestController {
     @PatchMapping("/{userId}/events/{eventId}/requests")
     public EventRequestStatusUpdateResultDto updateRequestStatus(@PathVariable Long userId,
                                                                  @PathVariable Long eventId,
-                                                                 @Validated @RequestBody(required=false) EventRequestStatusUpdateRequest eventRequestStatusUpdateRequest) {
+                                                                 @Validated @RequestBody(required = false) EventRequestStatusUpdateRequest eventRequestStatusUpdateRequest) {
         return requestService.updateRequestStatus(userId, eventId, eventRequestStatusUpdateRequest);
     }
 }
