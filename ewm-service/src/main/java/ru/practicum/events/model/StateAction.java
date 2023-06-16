18b57b7e447eb5f4bb74af6111d7eac049940fb2
Anna <annavvoronina@yandex.ru>
2023-06-16 21:38:30 +0300

    Этап 2. Основной сервис

--- ewm-service/src/main/java/ru/practicum/events/model/StateAction.java
+++ ewm-service/src/main/java/ru/practicum/events/model/StateAction.java
@@ -4,6 +4,26 @@ public enum StateAction {
     PUBLISH_EVENT, REJECT_EVENT, SEND_TO_REVIEW, CANCEL_REVIEW;
 
     public static StateAction stringToStateAction(String stringState) {
-        return StateAction.valueOf(stringState);
+        StateAction state;
+
+        try {
+            switch (StateAction.valueOf(stringState)) {
+                case PUBLISH_EVENT:
+                    state = PUBLISH_EVENT;
+                    break;
+                case CANCEL_REVIEW:
+                    state = CANCEL_REVIEW;
+                    break;
+                case REJECT_EVENT:
+                    state = REJECT_EVENT;
+                    break;
+                default:
+                    state = SEND_TO_REVIEW;
+            }
+        } catch (Exception exception) {
+            state = SEND_TO_REVIEW;
+        }
+
+        return state;
     }
 }
