3d5686156fe6fe8d7bcf85d9eae3543786508ea5
annavvoronina <annavvoronina@yandex.ru>
2023-06-20 15:24:10 +0300

    Этап 2. Основной сервис

--- stats-server/src/main/java/ru/practicum/StatServer.java
+++ stats-server/src/main/java/ru/practicum/StatServer.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StatServer {
     public static void main(String[] args) {
+        System.getProperties().put("server.port", 9090);
         SpringApplication.run(StatServer.class, args);
     }
 }
